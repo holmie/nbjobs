a7ac3915f1064550b8689a7ac08a962f78695957
Espen Holm Nilsen <espen@arpa.no>
2026-04-30 17:08:48 +0200

    Refactor HelloWorldJob registration in __init__.py

--- jobs/__init__.py
+++ jobs/__init__.py
@@ -1,12 +1,4 @@
-from nautobot.apps.jobs import Job
+from nautobot.apps.jobs import register_jobs
+from .hello import HelloWorldJob
 
-
-class HelloWorldJob(Job):
-    class Meta:
-        name = "Hello World"
-        description = "Minimal test-job for Nautobot/Celery"
-
-    def run(self, *args, **kwargs):
-        self.logger.info("Hello from nbjobs")
-        self.logger.info("Celery worker is alive")
-        return "Done"
+register_jobs(HelloWorldJob)
